fc63cd9341d9f951eb8a4d8803768ea1eddcaf4c
Disha Shah <sdisha3574@gmail.com>
2024-05-22 14:57:20 +0530

    Renamed to resolve openning issue

---
+++ admin/src/datatablesource.js
@@ -0,0 +1,48 @@
+export const userColumns = [
+  { field: "id", headerName: "ID", width: 70 },
+  {
+    field: "user",
+    headerName: "User",
+    width: 230,
+    renderCell: (params) => {
+      return (
+        <div className="cellWithImg">
+          <img className="cellImg" src={params.row.img || "https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg"} alt="avatar" />
+          {params.row.username}
+        </div>
+      );
+    },
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 230,
+  },
+  {
+    field: "country",
+    headerName: "Country",
+    width: 100,
+  },
+  {
+    field: "city",
+    headerName: "City",
+    width: 100,
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    width: 100,
+  },
+];
+
+export const userRows = [
+  {
+    id: 1,
+    username: "Snow",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    status: "active",
+    email: "1snow@gmail.com",
+    age: 35,
+  },
+  // Other user data...
+];
